b63d2629f4286caeaa7c18db0285311151a9cc9b
kumarmx <mukesh.s@gai.co.in>
2022-02-04 11:16:07 +0530

    add cahnges to .env file to changes the url of moodle webserver

--- src/app/services/env.service.ts
+++ src/app/services/env.service.ts
@@ -6,12 +6,12 @@ import { Injectable } from '@angular/core';
 export class EnvService {
   //Place all MOODLE_API_URL type Env Constants here
   public MOODLE_API_URL =
-    'https://chaavi.in/moodle/webservice/rest/server.php?moodlewsrestformat=json';
-  public MOODLE_LOGIN_URL = 'https://chaavi.in/moodle/login/token.php';
+    'https://moodle.gailabs.com/webservice/rest/server.php?moodlewsrestformat=json';
+  public MOODLE_LOGIN_URL = 'https://moodle.gailabs.com/login/token.php';
   public MOODLE_USER_ID: number;
   public MOODLE_USER_TOKEN: string;
   public MOODLE_FILE_UPLOAD_URL =
-    'https://chaavi.in/moodle/webservice/upload.php';
+    'https://moodle.gailabs.com/webservice/upload.php';
   public BBB_MEETING_INFO =
     'https://manager.bigbluemeeting.com/bigbluebutton/api/getMeetingInfo?';
   public BBB_MEETING_JOIN_INFO =
